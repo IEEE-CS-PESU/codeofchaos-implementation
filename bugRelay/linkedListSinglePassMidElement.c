3765264bccf1d9d5d29a43fa1246a55dcc4071d5
Sriram Radhakrishna <83068519+SR42-dev@users.noreply.github.com>
2022-04-16 19:48:10 +0530

    Update linkedListSinglePassMidElement.c

--- bugRelay/linkedListSinglePassMidElement.c
+++ bugRelay/linkedListSinglePassMidElement.c
@@ -1,18 +1,21 @@
-#include<conio.h>
+#include<conio.h> // likely to cause a runtime error as it's not a standard library
 #include<stdio.h>
 #include<stdlib.h>
+
 struct node{
 	int data;
 	struct node *next;
 	};
+
 struct node *head=NULL;
 struct node* createNode(){
 	struct node *newNode = (struct node *)malloc(sizeof(struct node));
-	return (newNode);
+	return (*newNode); // changed newNode to *newNode
 	}
 
 void insertNodeAtEnd(){
-	struct node *temp,*ptr;
+	struct node temp; // changed 'struct node* temp' to 'struct node temp'
+	struct node ptr; // changed 'struct node* ptr' to 'struct node ptr'
 	temp=createNode();
 	printf("enter the data you want to insert:");
 	scanf("%d",&temp->data);
@@ -21,7 +24,8 @@ void insertNodeAtEnd(){
 		head=temp;
 	else{
 		ptr=head;
-		while(ptr->next!=NULL){
+		while(ptr->next==NULL) // 'ptr->next != NULL' changed to 'ptr->next == NULL'
+		{
 			ptr=ptr->next;
 		}
 	       ptr->next=temp;
@@ -54,15 +58,15 @@ void findMiddle(){
     t=head;
     for (int i = 1; i < middle + 1; i++) 
         t = t->next; 
-    printf("middle number of list is :\n ");
+    printf("middle element :\n ");
     printf("%d",t->data);
 }
 
 int menu(){
 	int choice;
-	printf("\n 1.Add value to the list at end");
-	printf("\n 2.Travesre/View List");
-	printf("\n 3.Print Middle element of a list ");
+	printf("\n 1.Append to list");
+	printf("\n 2.View List");
+	printf("\n 3.Print Middle element of the list ");
 	printf("\n 4.exit");
 	printf("\n Please enter your choice: \t");
 	scanf("%d",&choice);
